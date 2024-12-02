b41ab86e52381a2d419005d8c93509a1cb5291d4
Alfonso Martinez Lopez <alfonso.m@cio.mx>
2024-12-02 13:23:38 -0600

    Se agrega el idioma ingles en los resources

--- app/src/main/java/com/example/appnutriologia/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/appnutriologia/ui/navigation/Screen.kt
@@ -7,7 +7,7 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Screen(val route:String, val title:String, val icon:ImageVector){
-    data object Home : Screen("home", "Inicio", Icons.Default.Home)
+    data object Home : Screen("home", "Home", Icons.Default.Home)
     data object Medidas : Screen("medidas", "Medidas", Icons.Default.Done)
     data object Plan : Screen("plan", "Plan", Icons.Default.Menu)
 }
